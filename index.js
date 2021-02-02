a106febdcea6c2a4e4becf8bea0db7c0cd45ebfa
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-01 22:04:01 -0500

    cors errors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 // 1. Require body-parser and save it to the variable parser.
 const parser = require('body-parser')
 const app = express()
+const cors = require('cors')
+const corsOptions = require("./configs/cors.js");
 
 const cookbookRouter = require('./controllers/cookbookRoutes')
 const authorRouter = require('./controllers/authorRoutes')
@@ -10,5 +12,6 @@ const authorRouter = require('./controllers/authorRoutes')
 app.use(parser.json())
 app.use('/api/cookbooks/', cookbookRouter)
 app.use('/api/authors/', authorRouter)
+app.use(cors({origin:'*'}))
 
 app.listen(4000, () => console.log('Server running on port 4000!'))
